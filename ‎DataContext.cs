3c8cb700eb566b3250b734e10f7a2999206b62aa
Abram <abram.perez795@gmail.com>
2025-11-10 21:33:57 -0600

    [1.09] Use appsettings.json

--- ‎DataContext.cs
+++ ‎DataContext.cs
@@ -13,13 +13,10 @@ public class DataContext : DbContext
 
   protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
   {
-    // *******************************************************************
-    // * IT IS NOT A GOOD IDEA TO STORE USER ID AND/OR PASSWORD          *
-    // * DIRECTLY IN A CLASS (ESPECIALLY IF IT WILL BE POSTED ON GITHUB) *
-    // * WE WILL LEARN LATER HOW TO REMOVE THIS DATA FROM THE CLASS      *
-    // *******************************************************************
-    // ## last 2 digits of CRN
-    // XXX your initials
-    optionsBuilder.UseSqlServer(@"Server=bitsql.wctc.edu;Database=Blogs_##_XXX;User ID=YYY;Password=ZZZ");
+  var configuration = new ConfigurationBuilder()
+            .AddJsonFile($"appsettings.json");
+
+    var config = configuration.Build();
+    optionsBuilder.UseSqlServer(@config["Blogs:ConnectionString"]);
   }
 }
