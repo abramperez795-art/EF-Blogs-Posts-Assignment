a571bf382cb07e8f79685dff6b587d1f09d0b5fe
Abram <abram.perez795@gmail.com>
2025-11-10 21:37:46 -0600

    [1.10] Menu

--- Program.cs
+++ Program.cs
@@ -4,24 +4,21 @@ string path = Directory.GetCurrentDirectory() + "//nlog.config";
 // create instance of Logger
 var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassLogger();
 
+var db = new DataContext();
+
 logger.Info("Program started");
 
-// Create and save a new Blog
-Console.Write("Enter a name for a new Blog: ");
-var name = Console.ReadLine();
-
-var blog = new Blog { Name = name };
-
-var db = new DataContext();
-db.AddBlog(blog);
-logger.Info("Blog added - {name}", name);
-
-// Display all Blogs from the database
-var query = db.Blogs.OrderBy(b => b.Name);
-
-Console.WriteLine("All blogs in the database:");
-foreach (var item in query)
+while (true)
 {
-  Console.WriteLine(item.Name);
+    Console.WriteLine("\nChoose an option:");
+    Console.WriteLine("1. Display all blogs");
+    Console.WriteLine("2. Add Blog");
+    Console.WriteLine("3. Create Post");
+    Console.WriteLine("4. Display Posts");
+
+    Console.Write("Enter option (1-4) or 'q' to Exit ");
+    string input = Console.ReadLine();
+
+
 }
 logger.Info("Program ended");
